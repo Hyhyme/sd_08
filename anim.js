0a22ad057369c8e6319f80f5c855c3e0195d29cc
Edward Luo <eluo1@stuy.edu>
2018-03-02 01:05:31 -0500

    finished

--- anim.js
+++ anim.js
@@ -5,50 +5,98 @@ var dvd = document.getElementById( 'dvdButt' );
 var stop = document.getElementById( 'stopButt' );
 var clear = document.getElementById( 'clearButt' );
 
-var r = 0;
+var time;
 
-var circle = function( r ) {
-    
-    var cl = document.createElementNS( ns, "circle" );
-    cl.setAttribute( 'cx', 250 );
-    cl.setAttribute( 'cy', 250 );
-    cl.setAttribute( 'r', r );
-    cl.setAttribute( 'fill', 'coral' );
-    cvs.appendChild(cl);
-}
 
 var growIt = function() {    
+
+    stopIt();
     
-    while( r < 250 ) {
-	time = setInterval( function(){ circle( r ) }, 100 ) ;
-	r++;
-	time++;
-	console.log(time);
+    var growing = true;
+    var r = 0;
+
+    var circle = function() {
+
+	clearIt();
+	
+	if( r >= 250 ) {
+	    growing = false;
+	}
+	
+	if(growing) {
+	    
+	    var cl = document.createElementNS( ns, "circle" );
+	    cl.setAttribute( 'cx', 250 );
+	    cl.setAttribute( 'cy', 250 );
+	    cl.setAttribute( 'r', r );
+	    cl.setAttribute( 'fill', 'coral' );
+	    cvs.appendChild(cl);
+	    r++;
+	    
+	} else {
+	    var cl = document.createElementNS( ns, "circle" );
+	    cl.setAttribute( 'cx', 250 );
+	    cl.setAttribute( 'cy', 250 );
+	    cl.setAttribute( 'r', r );
+	    cl.setAttribute( 'fill', 'coral' );
+	    cvs.appendChild(cl);
+	    r--;
+	    if( r <= 0 ){
+		growing = true;
+	    }
+	}
     }
-    clearInterval(time);
-    console.log(time);
+    time = setInterval(circle, 10);
 }
 
-var shrink = function() {
+var dvdIt = function() {
+
+    stopIt();
+
+    var currX = 250;
+    var currY = 250;
     
-    time = setInterval( function(){ circle(r) }, 1000 );
-    while( r > 0 ) {
-	r--;
-	console.log(r);
+    var angle = Math.PI / 6;
+    
+    var dX = Math.cos(angle) * 8;
+    var dY = Math.sin(angle) * 8;
+
+    var bouncing = function() {
+		
+	clearIt();
+	
+	var rec = document.createElementNS( ns, "rect" );
+	rec.setAttribute( 'x', currX );
+	rec.setAttribute( 'y', currY );
+	rec.setAttribute( 'width', 100 );
+	rec.setAttribute( 'height', 50 );
+	rec.setAttribute( 'fill', 'coral' );
+	cvs.appendChild(rec);
+	
+	if( currX <= 0 || currX >= 400 ) {
+	    dX *= -1;
+	}
+	if( currY <= 0 || currY >= 450 ) {
+	    dY *= -1;
+	}
+	currX += dX;
+	currY += dY;
+
     }
-    clearInterval(time);
-}	
+    time = setInterval( bouncing, 1000/60 );	
+}
 
 var clearIt = function() {
     while( cvs.lastChild ) {
 	cvs.removeChild( cvs.lastChild);
     }
-    start = false;
+}
+
+var stopIt = function() {
+    clearInterval(time);
 }
 
 grow.addEventListener( 'click', growIt );
-
-/*dvd.addEventListener( 'click', dvdIt );
+dvd.addEventListener( 'click', dvdIt );
 stop.addEventListener( 'click', stopIt );
 clear.addEventListener( 'click', clearIt );
-*/
